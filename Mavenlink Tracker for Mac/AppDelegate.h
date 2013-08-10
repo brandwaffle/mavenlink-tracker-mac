fb7b7b7f5fa264d8a1a35bcd61fa4a763352372f
Vasken Hauri <vhauri@gmail.com>
2013-08-09 22:05:24 -0700

    initial commit of the application with basic timer/stopwatch functionality

--- Mavenlink Tracker for Mac/AppDelegate.h
+++ Mavenlink Tracker for Mac/AppDelegate.h
@@ -11,5 +11,10 @@
 @interface AppDelegate : NSObject <NSApplicationDelegate>
 
 @property (assign) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSTextField *TextTimer;
+@property (weak) IBOutlet NSButton *reset;
+- (IBAction)push_start:(id)sender;
+- (IBAction)push_reset:(id)sender;
+- (void) tick:(id)sender;
 
 @end
--- Mavenlink Tracker for Mac/AppDelegate.m
+++ Mavenlink Tracker for Mac/AppDelegate.m
@@ -7,6 +7,9 @@
 //
 
 #import "AppDelegate.h"
+int second=0;
+int minute=0;
+BOOL started=TRUE;
 
 @implementation AppDelegate
 
@@ -15,4 +18,46 @@
     // Insert code here to initialize your application
 }
 
+- (IBAction)push_start:(id)sender {
+    if(started){
+        [_reset setTitle:@"Stop"];
+        started=FALSE;
+        [self tick:nil];
+    }else {
+        [_reset setTitle:@"Start"];
+        started=TRUE;
+    }
+}
+
+- (IBAction)push_reset:(id)sender {
+    second=0;
+    minute=0;
+    [_reset setTitle:@"Start"];
+    if(!started){
+        started=TRUE;
+    }
+    [_TextTimer setStringValue:@"00 : 00"];
+}
+
+- (void) tick:(id)sender{
+    if(!started){
+        NSString *sec=[NSString stringWithFormat:@"%i", second];
+        if(second<10){
+            sec=[NSString stringWithFormat:@"0%i", second];
+        }
+        NSString *min=[NSString stringWithFormat:@"%i", minute];
+        if(minute<10){
+            min=[NSString stringWithFormat:@"0%i", minute];
+        }
+        NSString *time=[NSString stringWithFormat:@"%@ : %@", min, sec];
+        [_TextTimer setStringValue:time];
+        [self performSelector:@selector(tick:) withObject:nil afterDelay:1.0];
+        second+=1;
+        if(second>59){
+            second=0;
+            minute+=1;
+        }
+        
+    }
+}
 @end
